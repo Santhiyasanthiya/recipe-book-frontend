bd95b77664c667d5f8165dfbafa6944d13c0d02b
santhiyasanthiya <santhiya30032@gmail.com>
2023-11-23 15:47:01 +0530

    er

--- src/components/menuData.js
+++ src/components/menuData.js
@@ -1,29 +1,28 @@
 export const MenuData = [
-    {
-        title: "Home",
-        url: "/home",
-        cName: "nav-links",
-        icon: "fas fa-house-user"
-    },
-    {
-        title: "Create Recipe",
-        url: "/create-recipe",
-        cName: "nav-links",
-        icon: "fa-solid fa-bowl-food"
-    },
-    {
-        title: "Saved Recipes",
-        url: "/saved-recipes",
-        cName: "nav-links",
-        icon: "fas fa-list"
-    },
-    {
-        title: "user"
-       
-    },
-    {
-        title: "Login/Signup",
-        url: "/",
-        cName: "nav-links-button",
-    },
+  {
+    title: "Home",
+    url: "/home",
+    cName: "nav-links",
+    icon: "fas fa-house-user",
+  },
+  {
+    title: "Create Recipe",
+    url: "/create-recipe",
+    cName: "nav-links",
+    icon: "fa-solid fa-bowl-food",
+  },
+  {
+    title: "Saved Recipes",
+    url: "/saved-recipes",
+    cName: "nav-links",
+    icon: "fas fa-list",
+  },
+  {
+    title: "user",
+  },
+  {
+    title: "Login/Signup",
+    url: "/",
+    cName: "nav-links-button",
+  },
 ];
